1f20d7de76874238f277ddee212223f0a8e9d71f
Rafael William <rafael.fim@hotmail.com>
2021-08-12 22:50:55 -0300

    click icon and hide menu

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* para o mobile, alterna o menu entre aberto e fechado */
 const nav = document.querySelector('#header nav')
 
 const toogle = document.querySelectorAll('nav .toogle')
@@ -5,6 +6,15 @@ const toogle = document.querySelectorAll('nav .toogle')
 for (const element of toogle) {
   element.addEventListener('click', () => {
     nav.classList.toggle('show')
+  })
+}
+
+/* ao clicar em um ícone do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
   })
 }
 
